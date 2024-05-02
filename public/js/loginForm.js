f8ad8a6ac90eff3f0d798881fa68be21545af07e
Kazaretski <s144084@ap.be>
2024-05-02 15:50:11 +0200

    here it is

--- public/js/loginForm.js
+++ public/js/loginForm.js
@@ -1,1 +1,12 @@
-// code goes here
+function UsernameInputChecker() {
+    if (document.querySelector("LogInForm input[type=text]") == "")  {
+        alert("Gelieve een geldige gebruikersnaam in te geven.");
+        event.preventDefault();
+    }
+}
+function PasswordInputChecker() {
+    if (document.querySelector("LogInForm input[type=password]") == "")  {
+        alert("Gelieve een geldig wachtwoord in te geven.");
+        event.preventDefault();
+    }
+}
